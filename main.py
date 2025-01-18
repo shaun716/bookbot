da0783b49b13c39c3037c3d9854623832a1c0615
dmcea13 <dmcea13@wgu.edu>
2025-01-18 17:46:17 -0500

    Adding comment to characterCount function

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        checker(file_contents)
+        wordCount(file_contents)
 
-def checker(file):
+def wordCount(file):
     words = file.split()
     print(len(words))
+
+def characterCount(file):
+    words = file.split()
+    """Add a new function to your script that takes the text from the book as a string, 
+    and returns the number of times each character appears in the string. 
+    Convert any character to lowercase, we don't want duplicates."""
+
 main()
