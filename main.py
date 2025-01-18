678b0aa1ca552a2602f1bac2705b9e421b8670e9
dmcea13 <dmcea13@wgu.edu>
2025-01-18 17:26:33 -0500

    Adding file word counter function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        checker(file_contents)
+
+def checker(file):
+    words = file.split()
+    print(len(words))
+main()
